698190cb7d104c980734e0c3dc5142d50767c80a
adam.obrusnik <adam.obrusnik@gmail.com>
2013-11-08 23:09:23 +0100

    test 3

--- integrate.py
+++ integrate.py
@@ -6,6 +6,9 @@ import xml.etree.ElementTree as ET
 from classes import *
 from functions import *
 
+if len(sys.argv) != 2:
+	raise IOError('Wrong number of arguments!!')
+
 eedf_filename = 'EEDFs.dat'
 eedfs_str = open(eedf_filename).read()
 eedfs = parseeedf(eedfs_str)
